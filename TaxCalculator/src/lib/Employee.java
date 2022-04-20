0c44f22f4e50618128ecbf00683e7c58a13f07c1
Ihsanasfari <ihsanasfari@student.telkomuniversity.ac.id>
2022-04-20 22:09:36 +0700

    restart

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -2,17 +2,10 @@ package lib;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-
 public class Employee {
-	
-	private enum JenisGender{
-		Lakilaki,
-		Perempuan
-	}
 
 	private String employeeId;
 	private String firstName;
@@ -20,11 +13,13 @@ public class Employee {
 	private String idNumber;
 	private String address;
 	
-	private Date tglJoined; 
+	private int yearJoined;
+	private int monthJoined;
+	private int dayJoined;
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private JenisGender gender; 
+	private boolean gender; //true = Laki-laki, false = Perempuan
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -36,13 +31,15 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, Date tglJoined, boolean isForeigner, JenisGender gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.tglJoined = tglJoined;
+		this.yearJoined = yearJoined;
+		this.monthJoined = monthJoined;
+		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
@@ -97,8 +94,8 @@ public class Employee {
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
-		if (date.getYear() == tglJoined.getYear()) {
-			monthWorkingInYear = date.getMonthValue() - tglJoined.getMonth();
+		if (date.getYear() == yearJoined) {
+			monthWorkingInYear = date.getMonthValue() - monthJoined;
 		}else {
 			monthWorkingInYear = 12;
 		}
---
+++ TaxCalculator/src/lib/Spouse.java
@@ -0,0 +1,14 @@
+package lib;
+
+public class Spouse {
+  
+        //data pasangan
+        private String spouseName;
+        private String spouseIdNumber;
+
+
+        public void setSpouse(String spouseName, String spouseIdNumber) {
+            this.spouseName = spouseName;
+            this.spouseIdNumber = idNumber;
+        }
+}
