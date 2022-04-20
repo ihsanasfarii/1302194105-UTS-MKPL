c3b73fa7d399e500f3f9bf1d746fa0d202a68a35
Ihsanasfari <ihsanasfari@student.telkomuniversity.ac.id>
2022-04-20 23:04:46 +0700

    memperbaiki Large class di spouse.java

--- TaxCalculator/src/lib/Spouse.java
+++ TaxCalculator/src/lib/Spouse.java
@@ -5,7 +5,7 @@ public class Spouse {
         //data pasangan
         private String spouseName;
         private String spouseIdNumber;
-        
+        public String idNumber;
 
         public void setSpouse(String spouseName, String spouseIdNumber) {
             this.spouseName = spouseName;
